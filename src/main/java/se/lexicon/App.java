7d9788ec97bc4c956fc4ac4d0f9b3a5c9770fc5e
Iffat-Sudo <zabin.ifa@gmail.com>
2025-10-30 15:03:49 +0100

    Added the all changes

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,8 +1,29 @@
 package se.lexicon;
 
 
+import se.lexicon.model.Book;
+import se.lexicon.model.Person;
+
 public class App {
     public static void main(String[] args) {
+        Book book1 = new Book("1984", "George Orwell");
+
+        // Step 2: Create a Person object
+        Person marcus = new Person("Marcus", "Andersson");
+
+        // Step 3: Marcus borrows the first book
+        marcus.loanBook(book1);
+
+        // Step 4: Show Marcus’s information
+        System.out.println(marcus.getPersonInformation());
+
+        // Step 5: Marcus returns the book
+        marcus.returnBook(book1);
+
+        // Step 6: Show Marcus’s information again
+        System.out.println(marcus.getPersonInformation());
+
+
 
     }
 
